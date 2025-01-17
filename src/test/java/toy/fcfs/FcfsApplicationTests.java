39228bb2619bd9d6ce3712b4fcca5b1fc8da66f1
Choi MinJung <mj020503@naver.com>
2025-01-17 15:46:07 +0900

    프로젝트 초기 환경 세팅

---
+++ src/test/java/toy/fcfs/FcfsApplicationTests.java
@@ -0,0 +1,13 @@
+package toy.fcfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
